d567c30649f4917c046b1336c16695bd206effb6
William Flanagan <wflanagan@Gibran.local>
2016-10-24 09:34:28 -0400

    Add config parameters for application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
     user_path(user)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :first_name, :last_name, :password, :password_confirmation, :email) }
   end
 
 end
